09040cbc2c5a4043b726f46c029d42726cac0b8c
6derick6 <6derick6@gmail.com>
2023-05-17 13:41:09 -0300

    Aba projetos

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, StatusBar, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar, ScrollView, StyleSheet, Dimensions, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useState, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
 
 
 function HomeScreen({navigation}) {
@@ -40,10 +42,76 @@ function SobreScreen({navigation}) {
   );
 }
 
-function PortfolioScreen({navigation}) {
+function PortfolioScreen({navigation,route}) {
+
+  const [images,setImages] = useState([
+    {
+      img: require('./resources/download.jpeg'),
+      width:0,
+      height:0,
+      ratio:0,
+      website:'https://github.com/6derick6/App_Portfolio'
+    },
+
+    {
+      img: require('./resources/download1.jpeg'),
+      width:0,
+      height:0,
+      ratio:0,
+      website:'https://github.com/6derick6/App_Portfolio'
+    }
+  ])
+
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+
+    let windowWidthN = Dimensions.get('window').width;
+
+    setWindowWidth(windowWidthN -30 -40);
+
+    let newImages = images.filter(function(val){
+      let w = Image.resolveAssetSource(val.img).width;
+      let h = Image.resolveAssetSource(val.img).height;
+
+      val.width = w;
+      val.height = h;
+
+      val.ratio = h/w;
+
+      return val;
+    })
+
+    setImages(newImages);
+
+  }, [])
+
+  const abrirNavegador = async (website) =>{
+    let result = await WebBrowser.openBrowserAsync(website);
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Portfolio Screen</Text>
+    <View style={{ flex: 1, padding:15}}>
+      <ScrollView contentContainerStyle={{padding:20}} style={styles.container}>
+        <Text style={styles.textHeader}>Os últimos projetos</Text>
+
+        {
+          images.map(function(val){
+            return(
+              <View style={styles.parentImage}>
+                <Image style={{width:windowWidth,height:windowWidth*val.ratio,resizeMode:'stretch'}} source={val.img} />
+
+                <TouchableOpacity onPress={()=>abrirNavegador(val.website)} style={styles.botaoAbrirNavegador}>
+                  <Text style={{textAlign:'center',color:'white',fontSize:18}}>Abrir no navegador!</Text>
+                </TouchableOpacity>
+
+              </View>
+            )
+          })
+        }
+
+      </ScrollView>
+
     </View>
   );
 }
@@ -104,5 +172,14 @@ const styles = StyleSheet.create({
     padding:20,
     marginTop:15,
     flexDirection:'row'
+  }, 
+
+  parentImage: {
+    marginTop:30
+  },
+
+  botaoAbrirNavegador: {
+    padding:10,
+    backgroundColor:'#5f5380',
   }
 })
